0e243e872de7f4bc1274473d0f95a5ce09b8b676
Angel Motta <angelmotta@gmail.com>
2019-03-27 12:55:21 -0500

    Se agrega funcionalidad de contar letras del nombre. Programming is Art

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,15 @@
 import sys
 import os
 
-nombre = input()
+dict = {}
+nombre = input("Ingresar nombre: ")
 print("HOLA ..."+nombre)
+for letter in nombre:
+    if letter in dict.keys():
+        dict[letter] = dict.get(letter) + 1    
+    else:
+        dict[letter] = 1
+
+for key in dict:
+    print(key + " --> " + str(dict[key]))
+
