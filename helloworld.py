62dedabc8a33c9ae06e380b6771664bfe1113f2b
Jorge Ernesto Nicho Galagarza <jorge.nicho@utec.edu.pe>
2019-03-27 13:23:55 -0500

    Archivo py jorge

--- helloworld.py
+++ helloworld.py
@@ -2,4 +2,18 @@ import sys
 import os
 
 nombre = input()
-print("HOLA ..."+nombre)
+count=0
+long = len(nombre)
+lista = []
+for x in nombre:
+    if x in lista:
+        continue
+    else:
+        lista.append(x)
+    count2 = 0
+    vecesRepetidas = 0
+    while count2 < long:
+        if x == nombre[count2]:
+            vecesRepetidas +=1
+        count2 +=1
+    print(x + "=" + str(vecesRepetidas))
