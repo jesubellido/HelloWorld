01d798d18bc87b5f76621ff9643cd909336d8694
victor812 <victorpa793@gmail.com>
2019-03-27 14:30:22 -0500

    Update helloworld.py
    Trabajo de clase

--- helloworld.py
+++ helloworld.py
@@ -1,26 +1,21 @@
-import sys
-import os
+palabra = input("ingrese nombre")
+longitud = len(palabra)
+contador = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+letras = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z']
+i=0
+j=0
 
-Juan-Galvez
-sum=0
-nombre = raw_input() 
-for i in nombre:
-    for j in nombre:
-        if i==j:
-           sum=sum+1
-    print(i+" "+str(sum))
-    sum=0
-=======
-nombre =raw_input("Como te llamas?")
-contador = 0
-letra = ""
-for c in nombre:
-    for j in nombre:
-        if c==j:
-            contador += 1
-    contador=0
-    print(c+" se repite "+str(contador)+" veces." )
+while i<27:
+    j=0
+    while j<longitud:
+       if letras[i]== palabra[j]:
+            contador[i]=contador[i]+1
+
+       j+=1
+    i+=1
 
 
+for i in range (0,26,1):
+    if contador[i]!=0:
+        print(letras[i],"tiene ",contador[i])
 
- master
