8286cf0983fe1ac5d4c87afb23139a70f6bb690f
fabriziofranco <fabrizio.franco@utec.edu.peUt3c2675clearREADME.md it config --global user.email fabrizio.franco@utec.edu.pe>
2019-03-27 13:21:20 -0500

    mi version del programa

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,17 @@
 import sys
 import os
+i=0
+Lista=[]
+Listalimpia=[]
+nombre = input("Ingresa tu nombre: ")
+while i<len(nombre):
+    Lista.append(nombre[i])
+    i+=1
+for j in Lista:
+    if j not in Listalimpia:
+        Listalimpia.append(j)
+for g in Listalimpia:
+    print("La letra ",g, " aparece ",nombre.count(g), " veces")
 
-nombre = input("Como te llamas?")
-print("Hola" ,nombre)
+
+#Escrito por Fabrizio Franco utilizando PYTHON 3 (ejecutar con dicha version)
