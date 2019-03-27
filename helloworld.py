b41adcf0ffa892cabf3f7bb588daf395d5b48203
stephanoFW <stephanoFW@gmail.com>
2019-03-27 12:46:13 -0500

    agregue mi nombre y un poco de codigo

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,6 @@
 import sys
 import os
 
-nombre = input()
-print("HOLA ..."+nombre)
+print ("Soy Stephano Wurttele, pero igual preguntare el nombre:")
+nombre = input("Como te llamas?")
+print("Hola" ,nombre)
