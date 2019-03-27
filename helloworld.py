08206492a66a114110c48947eaf4aa8b1ed4b38b
AejandroGH97 <alejandrogoicochea@gmail.com>
2019-03-27 12:41:19 -0500

    Se agrego la funcionalidad a helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -2,4 +2,9 @@ import sys
 import os
 
 nombre = input()
-print("HOLA ..."+nombre)
+numero_de_repeticiones = 0
+for letra in nombre:
+    if letra.upper() == 'A':
+        numero_de_repeticiones+=1
+
+print("La letra 'a' se repite " + str(numero_de_repeticiones) + " veces.")
