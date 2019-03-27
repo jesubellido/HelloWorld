f746ea0c37acae5549fe38d65b63c57ba0bc9f3d
Enrique <enrique.sobrados@utec.edu.pe>
2019-03-27 13:03:17 -0500

    cambio que pidio el profe

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,21 @@
-import sys
-import os
+import sys 
+import os 
+nombre=input("Ingrese su nombre: ")
+longitud=len(nombre)
+letras=[]
 
-nombre = input()
-print("HOLA ..."+nombre)
+for i in range(0,longitud,1):
+    letras.append(nombre[i])
+for j in range(0,longitud,1):
+    a= nombre[j]
+    b=0
+    for k in range(0,longitud,1):
+        if a == nombre[k]:
+            b=b+1
+    print("La letra ", nombre[j] ," se repite ", b ,"veces")
+
+
+
+
+
+
