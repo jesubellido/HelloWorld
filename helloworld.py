989cb76c7de85efc36391590a598ab223b6121ba
BenjaminDiazGarcia <benjamin.diaz@utec.edu.pe>
2019-03-27 12:59:16 -0500

    modificacion

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,15 @@
 import sys
 import os
 
-nombre = input("Como te llamas?")
+nombre = input("Como te llamas: ")
 print("Hola" ,nombre)
+
+contadordeletras={}
+
+for letra in nombre:
+    if letra in contadordeletras:
+        contadordeletras[letra]=contadordeletras[letra]+1
+    else:
+        contadordeletras[letra]=1
+
+print(contadordeletras)
