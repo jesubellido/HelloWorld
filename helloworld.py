2cefbaa477171e87410229b6b7116e69df0294c9
Fgarcia19 <40769836+Fgarcia19@users.noreply.github.com>
2019-03-27 15:28:54 -0500

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,11 @@
 import sys
 import os
 
-nombre = input("Como te llamas?")
-print("Hola" ,nombre)
-dada
+nombre=input()
+suma=0
+for i in nombre:
+    for j in nombre:
+        if i == j:
+            suma=suma+1
+    print(i, suma)
+    suma=0
