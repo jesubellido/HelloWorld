70553b0c22319247450336c8ef39cd37577b292d
fresh-avocado <gabriel.spranger@utec.edu.pe>
2019-03-27 13:03:38 -0500

    Añadiendo helloworld.py actualizado.

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,15 @@
 import sys
 import os
 
-nombre = input("Como te llamas?")
-print("Hola" ,nombre)
+nombre =raw_input("Como te llamas?")
+contador = 0
+letra = ""
+for c in nombre:
+    for j in nombre:
+        if c==j:
+            contador += 1
+    contador=0
+    print(c+" se repite "+str(contador)+" veces." )
+
+
+
