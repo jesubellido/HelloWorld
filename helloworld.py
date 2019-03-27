35558a884ce9d7d016a96201c3a1187770a4ec56
utec <utec@utec.edu.pe>
2019-03-27 13:13:49 -0500

    Añadiendo archivo actualizado, contador de letras en nombre

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,14 @@
 import sys
 import os
 
-nombre = input("Como te llamas?")
-print("Hola" ,nombre)
+print ("Como te llamas?")
+nombre = raw_input()
+
+contador=0
+for letra in nombre:
+    for letrar in nombre:
+        if letra==letrar:
+             contador+=1
+    print(letra+" "+str(contador))
+    contador=0
+
