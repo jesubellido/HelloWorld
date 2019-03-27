02a8cdc1aa5ab58f90d161847132e6ba1489ba5f
AejandroGH97 <alejandrogoicochea@gmail.com>
2019-03-27 14:14:17 -0500

    Se cambio la funcionalidad de contar las apariciones de la letra A a todas las del abecedario

--- helloworld.py
+++ helloworld.py
@@ -2,9 +2,12 @@ import sys
 import os
 
 nombre = input()
-numero_de_repeticiones = 0
+numero_de_letras = {}
 for letra in nombre:
-    if letra.upper() == 'A':
-        numero_de_repeticiones+=1
+    if letra.upper() in numero_de_letras:
+        numero_de_letras[letra.upper()]+=1
+    else:
+        numero_de_letras[letra.upper()] = 1
 
-print("La letra 'a' se repite " + str(numero_de_repeticiones) + " veces.")
+for letra in numero_de_letras:
+    print("La letra " + letra + " se repite " + str(numero_de_letras[letra]) + " veces.")
