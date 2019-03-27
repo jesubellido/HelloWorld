2430fe88ca4adbdd052dbef6271d87a75cf2eec2
cesar21456 <39863125+cesar21456@users.noreply.github.com>
2019-03-27 12:46:16 -0500

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -2,4 +2,15 @@ import sys
 import os
 
 nombre = input()
-print("HOLA ..."+nombre)
+letras=[]
+contadores=[]
+for i in nombre:
+  if i in letras:
+    x=letras.index(i)
+    contadores[x]+=1
+    pass
+  else:
+    letras.append(i)
+    contadores.append(int(1))
+for j in range(len(letras)):
+  print(letras[j],"fue escrito",contadores[j],"veces")
