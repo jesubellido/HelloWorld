8aefa81fe3ca6dca13f3b442d33a2a610c053a14
luiggiroal <luiggi.rodriguez@utec.edu.pe>
2019-03-27 13:22:57 -0500

    Se lee el numero de letras del nombre

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,12 @@
 import sys
 import os
 
-nombre = input("Como te llamas?")
-print("Hola" ,nombre)
+
+nombre = input('Enter your name:')
+for x in nombre:
+	count = 0
+	for i in nombre:
+		if x == i:
+			count += 1	
+	print('The letter', x, 'repeats', count, 'time(s)')
+# print("HOLA ..."+nombre)
