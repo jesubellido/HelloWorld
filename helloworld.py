d72e5f69c2590a29a587235e4ee256583e055512
Renato Bacigalupo Ortiz-Arrieta <renato.bacigalupo@utec.edu.pe>
2019-03-27 13:08:04 -0500

    Nombre y Letras

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,13 @@
 import sys
 import os
 
-nombre = input("Como te llamas?")
-print("Hola" ,nombre)
+print("Hola...")
+Nombre = input("Cual es su nombre? : ")
+print("Hola ")
+print(Nombre)
+for x in Nombre-1:
+    rep = 0
+    for x in Nombre-1:
+        if x == x+1:
+            rep += 1
+    print(x + "=" + rep)
